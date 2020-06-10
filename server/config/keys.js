eb66d9918b2a1ce6612b4e446db826181ab481d6
Gerson Espinal <gerson.espinal@gmail.com>
2020-06-10 17:13:36 -0400

    connection to mongodb atlas established

---
+++ server/config/keys.js
@@ -0,0 +1,8 @@
+const path = require("path");
+require("dotenv").config({
+  path: path.join(__dirname, "..", ".env"),
+});
+
+module.exports = {
+  mongoUri: process.env.MONGO_URI,
+};
---
+++ server/config/mongo.js
@@ -0,0 +1,34 @@
+const MongoClient = require("mongodb").MongoClient;
+const { mongoUri } = require("./keys");
+
+async function main() {
+  const uri = mongoUri;
+
+  const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  };
+
+  const client = new MongoClient(uri, options);
+
+  try {
+    await client.connect();
+
+    // TODO Remove
+    await listDatabases(client);
+  } catch (err) {
+    console.error(err);
+  } finally {
+    await client.close();
+  }
+}
+
+main().catch(console.error);
+
+// TODO Remove
+async function listDatabases(client) {
+  databasesList = await client.db().admin().listDatabases();
+
+  console.log("Databases:");
+  databasesList.databases.forEach((db) => console.log(` - ${db.name}`));
+}
