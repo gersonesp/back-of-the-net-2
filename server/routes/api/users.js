e485f92c79a926ed4a26c06563f85f6cfa8221a8
Gerson Espinal <gerson.espinal@gmail.com>
2020-06-11 15:11:59 -0400

    Added login route

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const keys = require("../../config/keys");
+const {passportSecret} = require("../../config/keys");
 
 // Load input validation
 const validateRegisterInput = require("../../validation/register");
@@ -24,7 +24,7 @@ router.post("/register", async (req, res) => {
     const user = await User.findOne({ email: req.body.email });
 
     if (user) {
-      return res.status(400).send({ email: "Email already exists" });
+      return res.status(400).send({ response: "Email already exists" });
     } else {
       const newUser = new User({
         name: req.body.name,
@@ -43,5 +43,51 @@ router.post("/register", async (req, res) => {
     }
   } catch (err) {
     console.error(err);
+    res.status(500).end();
   }
 });
+
+router.post("/login", async (req, res) => {
+  // Form validation
+  const {errors, isValid} = validateLoginInput(req.body)
+
+  // Check validation
+  if(!isValid) {
+    return res.status(400).send(errors)
+  }
+
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Find user by email
+  try {
+    const user = await User.findOne({email})
+
+    if(!user) {
+      return res.status(404).send({response: "Email not found"})
+    }
+
+    // Check password
+    const hash = user.password;
+    const match = await bcrypt.compare(password, user.password)
+    if(!match) {
+      res.status(401).send({ response: "Password incorrect" })
+    } else {
+      // User matched
+      // Create JWT Payload
+      const payload = {
+        id: user.id,
+        name: user.name
+      };
+
+      // Sign token
+      const token = jwt.sign(payload, passportSecret);
+      res.status(200).send({token, user: payload})
+    }
+  } catch (err) {
+    console.error(err)
+    res.status(500).end();
+  }
+})
+
+module.exports = router;
