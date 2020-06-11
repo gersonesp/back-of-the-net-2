1a94b62a3979bac27a4e846fb91a8b02b8da303e
Gerson Espinal <gerson.espinal@gmail.com>
2020-06-11 17:57:27 -0400

    Cleaned up code

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,28 +1,5 @@
-// const MongoClient = require("mongodb").MongoClient;
 const mongoose = require("mongoose");
 const { mongoUri } = require("./keys");
-
-// async function main() {
-//   const uri = mongoUri;
-
-//   const options = {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   };
-
-//   const client = new MongoClient(uri, options);
-
-//   try {
-//     await client.connect();
-//     console.log("Connected to cluster!!");
-//   } catch (err) {
-//     console.error(err);
-//   } finally {
-//     await client.close();
-//   }
-// }
-
-// main().catch(console.error);
 
 const options = {
   useNewUrlParser: true,
