c023d7fe5cd0d3900ca1322a58b97d4ba32e0ac7
Gerson Espinal <gerson.espinal@gmail.com>
2020-09-10 14:26:55 -0400

    updated test

--- server/test/predictions.spec.js
+++ server/test/predictions.spec.js
@@ -9,7 +9,7 @@ describe("/GET predictions", () => {
   it("it should return 200", (done) => {
     chai
       .request(app)
-      .get(`/predicaitons/`)
+      .get(`/api/predictions/`)
       .end((err, res) => {
         res.should.have.status(200);
         done();
