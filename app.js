101033455e728db9d7311d2091e18ac64d3549fd
Gerson Espinal <gerson.espinal@gmail.com>
2020-09-11 15:56:42 -0400

    clean up

--- app.js
+++ app.js
@@ -24,11 +24,15 @@ app.use(logger("dev"));
 app.use(json());
 app.use(urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(join(__dirname, "build")));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
+// development
+app.use(express.static(join(__dirname, "public")));
+
+// production
+// app.use(express.static(join(__dirname, "build")));
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "build", "index.html"));
+// });
 
 // Passport middleware
 app.use(passport.initialize());
